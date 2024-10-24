738a656877c0092110d19618e99d8a4d87f0e5a6
daren-scerri <daren.scerri@mcast.edu.mt>
2024-10-24 08:40:06 +0200

    Association lecture Part 2 - shapes bouncing and Square using ShapeMovement and Serializefield parameters

--- Association Lecture/Assets/Scripts/Circle.cs
+++ Association Lecture/Assets/Scripts/Circle.cs
@@ -6,11 +6,14 @@ public class Circle : MonoBehaviour
 {
     Rigidbody2D circleRB;
     [SerializeField] float yspeed=1;
+    [SerializeField] float starting_Xpos = 0;
+    [SerializeField] float starting_Ypos = 0;
 
-    ShapeMovement myCircleMovement = new ShapeMovement(0f, -3f, 0f, 1f);
+    ShapeMovement myCircleMovement;
     // Start is called before the first frame update
     void Start()
     {
+        myCircleMovement = new ShapeMovement(starting_Xpos, starting_Ypos, 0f, yspeed);
         circleRB = GetComponent<Rigidbody2D>();
         myCircleMovement.ResetPosition(circleRB);
 
--- Association Lecture/Assets/Scripts/Square.cs
+++ Association Lecture/Assets/Scripts/Square.cs
@@ -6,23 +6,23 @@ public class Square : MonoBehaviour
 {
     Rigidbody2D squareRB;
     [SerializeField] float xspeed = 1;
+    [SerializeField] float starting_Xpos = 0;
+    [SerializeField] float starting_Ypos = 0;
+
+    ShapeMovement mySquareMovement;
     // Start is called before the first frame update
     void Start()
     {
+        mySquareMovement = new ShapeMovement(starting_Xpos, starting_Ypos, xspeed, 0f);
         squareRB = GetComponent<Rigidbody2D>();
+        mySquareMovement.ResetPosition(squareRB);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            squareRB.velocity = new Vector2(-xspeed, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            squareRB.velocity = new Vector2(xspeed, 0);
-        }
+        mySquareMovement.MoveLeft(squareRB);
+        mySquareMovement.MoveRight(squareRB);
 
 
     }
